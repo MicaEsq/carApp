a4599197ccdf138ba120ccbfd3eb8542cd6c307c
MicaEsq <micaesquerdo1@gmail.com>
2023-05-24 21:56:22 -0300

    Filter by input half functionality

--- frontend/src/components/Filters.js
+++ frontend/src/components/Filters.js
@@ -4,6 +4,10 @@ import { useState, useEffect } from 'react'
 function getButtons(options, getFilters, setFiltersApplied, filtersApplied, setModifiedFilter){
   const [minimum, setMinimum] = useState('');
   const [maximum, setMaximum] = useState('');
+  const [from, setFrom] = useState('');
+  const [to, setTo] = useState('');
+  const [mileage, setMileage] = useState('');
+  let debounceTimer='';
 
   var newSetOptions = filtersApplied.slice();
   
@@ -23,74 +27,143 @@ function getButtons(options, getFilters, setFiltersApplied, filtersApplied, setM
     else if(options.type === 'states'){ 
       getFilters('cities', idPrimary)
     }
-    else{
+  }
+
+
+  function handleChange(event){ //terminar de ver tema filtros de input
+    
+    clearTimeout(debounceTimer);
+
+    const id = event.target.id;
+    const value = event.target.value;
+    const regex = /^[0-9\b]+$/;
+    if (event.target.value === "" || regex.test(event.target.value)) {
+      if(id === 'From'){
+        setFrom(value)
+      }
+      else if( id === 'To'){
+        setTo(value)
+      }
+      else if(id === 'Minimum'){
+        setMinimum(value)
+      }
+      else if(id === 'Maximum'){
+        setMaximum(value)
+      }
+      else{
+        setMileage(value)
+      }
+      
     }
-  }
+    else{
+      if(id === 'Mileage'){
+        setMileage(value)
+      }
+    }
 
-  const handleChangeMinimum = evt => {
-    const value = evt.target.value
-    setMinimum(value)
+    debounceTimer = setTimeout(() => {
+     
+      const regex = /^[0-9\b]+$/;
+      if (event.target.value === "" || regex.test(event.target.value)) {
+        if(id === 'From'){
+          addToSet(value, options.type, 'From')
+        }
+        else if( id === 'To'){
+          addToSet(value, options.type, 'To')
+        }
+        else if(id === 'Minimum'){
+          addToSet(value, options.type, 'Minimum')
+        }
+        else if(id === 'Maximum'){
+          addToSet(value, options.type, 'Maximum')
+        }
+        else{
+          addToSet(value, options.type, 'Mileage')
+        }
+        
+      }
+      else{
+        if(id === 'Mileage'){
+          addToSet(value, options.type, 'Mileage')
+        }
+      }
+    }, 1000); // Adjust the debounce delay as needed (e.g., 300ms)
   }
-
-  const handleChangeMaximum = evt => {
-    const value = evt.target.value
-    setMaximum(value)
-  }
+  
   
   if(options.type === 'states' || options.type === 'cities' || options.type === 'brands' || options.type === 'models' || options.type === 'transmission'){
     return options.values.map((option, index) => {
-        return <Disclosure.Button key={index} className='text-[#1B2141] pb-4 text-left' onClick={()=>{addToSet(option.name, options.type); checkGet(option.id)}}>{option.name}</Disclosure.Button>
+        return <Disclosure.Button key={index} className='text-[#1B2141] pb-4 text-left ml-4' onClick={()=>{addToSet(option.name, options.type); checkGet(option.id)}}>{option.name}</Disclosure.Button>
     } )
   }
-  else{
-        return <div className="flex flex-row">{/*  //modify depending on filter */}
-          
-            <div className="relative my-6">
+  else if(options.type === 'year' || options.type === 'price' ){
+
+        let leftBox = '';
+        let rightBox = '';
+        if(options.type === 'year'){
+          leftBox = 'From';
+          rightBox = 'To';
+        }
+        else{
+          leftBox = 'Minimum';
+          rightBox = 'Maximum';
+        }
+
+        return <div className="flex flex-row ml-2">
+            <div className="relative mb-2 mx-2">
               <input
-                id="min"
-                type="text"
-                placeholder="Minimum"
-                value={minimum}
-                className="peer relative h-10 w-full border-b border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white focus:border-indigo-500 focus:outline-none focus-visible:outline-none "
-                onChange={handleChangeMinimum}
+                id={leftBox}
+                placeholder={leftBox}
+                value={options.type === 'price' ? minimum : from}
+                className="peer relative h-10 w-full border rounded-lg border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white focus:border-indigo-500 focus:outline-none focus-visible:outline-none "
+                onChange={handleChange}
               />
               <label
-                htmlFor="min"
+                htmlFor={leftBox}
                 className="absolute left-2 -top-2 z-[1] cursor-text  text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm"
               >
-                Min.
+                {leftBox}
               </label>
             </div>
-            <div className="relative my-6">
+            <div className="relative mb-2 mx-2">
               <input
-                id="max"
-                type="text"
-                placeholder="Maximum"
-                value={maximum}
-                className="peer relative h-10 w-full border-b border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white focus:border-indigo-500 focus:outline-none focus-visible:outline-none "
-                onChange={handleChangeMaximum}
+                id={rightBox}
+                placeholder={rightBox}
+                value={options.type === 'price' ? maximum : to}
+                className="peer relative h-10 w-full border rounded-lg border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white focus:border-indigo-500 focus:outline-none focus-visible:outline-none "
+                onChange={handleChange}
               />
               <label
-                htmlFor="max"
+                htmlFor={rightBox}
                 className="absolute left-2 -top-2 z-[1] cursor-text  text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm"
               >
-                Max.
+                {rightBox}
               </label>
             </div>
       </div>
-      }
+    }
+    else{
+      return options.values.map((option, optionIdx) => {
+        return <div key={option.value} className="flex items-center ml-4 my-2">
+            <input type="radio" id={option.value} name="radiobutton" value={option.value}
+                  className="form-radio h-4 w-4 accent-indigo-500 p-3 my-2 mr-2" onClick={handleChange}/>
+            <label htmlFor={option.value}>{option.value}</label>
+        </div>
+      })
+    }
     
 }
 
 export default function Filters({setFiltersApplied, filtersApplied, setModifiedFilter}) {
 
   const [filters, setFilters] = useState(['brand', 'model', 'year', 'state', 'city', 'transmission', 'price', 'mileage']);
-  const [filters2, setFilters2] = useState([{type:'brands', values:[]}, {type:'models', values:[]}, {type:'year', values:[]}, {type:'states', values:[]}, {type:'cities', values:[]}, {type:'transmission', values:[{id:1, name:'Automatic'}, {id:2, name:'Manual'}]}, {type:'price', values:[]}, {type:'mileage', values:[]}]);
+  const [filters2, setFilters2] = useState([{type:'brands', values:[]}, {type:'models', values:[]}, {type:'year', values:[]}, {type:'states', values:[]}, {type:'cities', values:[]}, {type:'transmission', values:[{id:1, name:'Automatic'}, {id:2, name:'Manual'}]}, {type:'price', values:[]}, {type:'mileage', values:[{id:1, value:'35.000 or less'}, {id:2, value:'35.000 to 60.000 km'}, {id:3, value:'60.000 to 80.000 km'}, {id:4, value:'80.000 to 100.000'}, {id:5, value:'100.000 km or more'}]}]);
   
   useEffect(() => {
     
     getFilters('brands', '')
     getFilters('states', '')
+
 
   }, [])
 
@@ -132,10 +205,10 @@ export default function Filters({setFiltersApplied, filtersApplied, setModifiedF
   return (
     <div className="flex flex-col bg-white mt-16 rounded-lg">
       {filters.map((filter, index) => {
-        return <Disclosure key={filter.type}>
+        return <Disclosure key={filter}>
           {({ open }) => ( 
             <>
-            <Disclosure.Button className="grid grid-rows-1 grid-flow-col items-center text-left py-5 text-[#1B2141] font-bold">
+            <Disclosure.Button  className="grid grid-rows-1 grid-flow-col items-center text-left py-5 text-[#1B2141] font-bold">
               {filter[0].toUpperCase() + filter.substring(1)}
               <div className={open ? 'justify-self-end rotate-180 transform' : 'justify-self-end'} >
                 <svg width="10" height="6" viewBox="0 0 10 6" fill="#5F647A" xmlns="http://www.w3.org/2000/svg">
